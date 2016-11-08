5683fbd0ab8e70f7af45ed4d4b9ccf829a4938a9
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-11-08 08:50:39 -0600

    Add products route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var exphbs = require('express-handlebars');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var products = require('./routes/products');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/products', products);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
