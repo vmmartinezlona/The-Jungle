5a6ce74d8b313ea2c28fdbd37c8a0e1c5c788377
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-11-08 08:55:33 -0600

    Update products view

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'the jungle' });
+  res.render('products', { title: 'PRODUCTS' });
 });
 
 module.exports = router;
