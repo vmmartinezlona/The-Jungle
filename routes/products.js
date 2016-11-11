649064ed13a18df8994907b1805f6dcf815ff1c9
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-11-11 09:45:04 -0600

    Add Products Query

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,31 @@ var Product = require('../models/Product.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	/*
   var animalList = ['cat', 'doc', 'bird', 'bear'];
   res.render('products', {
     title: 'PRODUCTS',
     animals: animalList
   });
+  */
+
+  Product.find(function(error, productList) {
+
+  	if(error) {
+  		res.send(500, error.message);
+  	}
+
+  	res.render('products', {
+  		title: 'PRODUCTS',
+  		products: productList
+  	});
+
+
+  });
+
+
+
+
 });
 
 router.post('/', function(req, res) {
